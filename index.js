f05e174f49e2da7a1c296ed5788b8d8d306bb2d5
Mattegbg <90768274+Mattegbg@users.noreply.github.com>
2022-05-13 19:01:51 +0200

    planeraren

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const API_TOKEN = 'pk.eyJ1Ijoiam9oYW5raXZpIiwiYSI6ImNrcnl6M25xMDA4aWUyd3BqY3EzYn
 
 const buttonElem = document.querySelector('#position-button');
 
-
 function showOnMap(position) {
     mapboxgl.accessToken = API_TOKEN;
-        const map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v11',
-            center: [position.coords.longitude, position.coords.latitude],
-            zoom: 13
-        });
+    const map = new mapboxgl.Map({
+        container: 'map',
+        style: 'mapbox://styles/mapbox/streets-v11',
+        center: [position.coords.longitude, position.coords.latitude],
+        zoom: 13
+    });
+
     new mapboxgl.Marker().
     setLngLat([position.coords.longitude, position.coords.latitude]).
     addTo(map);
@@ -18,11 +18,23 @@ function showOnMap(position) {
 
 
 buttonElem.addEventListener('click', () => {
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      console.log(position);
-      showOnMap(position);
-
-    });
-  }
+    if (navigator.geolocation) {
+       navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position);
+        showOnMap(position);
+       });
+    }
 });
+
+
+//Planeraren hitta hållplatser nedan?
+
+async function getStops() {
+  const response = await fetch('https://api.resrobot.se/v2.1/location.nearbystops?input=Göteborg&format=json&accessId=API_KEY');
+  const data = await response.json();
+
+  console.log(data);
+ 
+}
+
+getStops();
